f257ad90eda1a244c1ecab16ba74cb0c733b0e3a
Lucca <lucca.chen@crpusd.org>
2025-11-10 10:15:33 -0800

    YAYAY

--- 1214.py
+++ 1214.py
@@ -4,13 +4,32 @@ import tkinter as tk
 
 # main window
 root = tk.Tk()
-root.wm_geometry("200x100")
+root.wm_geometry("200x200")
 root.title('Authentication')
 # create empty frame
+def Login(password, username):
+    if username == "Genshin" and password == "Impact":
+        print("Login success!")
+    else:
+        print("wrong")
+def get_text():
+    password = password_entry.get()
+    username = username_entry.get()
+    Login(password, username)
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_login.grid(column=0, row=0)
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
+username = ""
+username_entry = tk.Entry(frame_login, width=7, textvariable=username)
+username_entry.pack()
+lbl_password = tk.Label(frame_login,text="Password:",font="Courier")
+lbl_password.pack()
+password = ""
+password_entry = tk.Entry(frame_login, width=7, textvariable=password)
+password_entry.pack()
 
+Enter = tk.Button(frame_login, text="Login", command=get_text)
 
+Enter.pack()
 root.mainloop()
