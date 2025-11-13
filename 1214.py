9c467b477537409dcbfe10fae95ab2e1f1be5c94
Pianocraft1YT <lucca.chen@crpusd.org>
2025-11-13 09:49:34 -0800

    DONE

--- 1214.py
+++ 1214.py
@@ -7,11 +7,31 @@ root = tk.Tk()
 root.wm_geometry("200x200")
 root.title('Authentication')
 # create empty frame
+def showNewCorrect():
+    root.withdraw()
+    root2 = tk.Tk()
+    root2.wm_geometry("200x200")
+    root2.title("SUCCESS!")
+    frame_correct = tk.Frame(root2)
+    frame_correct.grid()
+    lbl_correct = tk.Label(frame_correct, text="CORRECT!", font="Times, 20")
+    lbl_correct.pack()
+def showNewFrame(): 
+    root.withdraw()
+    root1 = tk.Tk()
+    root1.wm_geometry("200x200")
+    root1.title("WRONG!")
+    
+    frame_wrong = tk.Frame(root1)
+    frame_wrong.grid()
+    
+    lbl_wrong = tk.Label(frame_wrong, text="WRONG!", font="Times, 26")
+    lbl_wrong.pack()
 def Login(password, username):
     if username == "Genshin" and password == "Impact":
-        print("Login success!")
+        showNewCorrect()
     else:
-        print("wrong")
+        showNewFrame()
 def get_text():
     password = password_entry.get()
     username = username_entry.get()
