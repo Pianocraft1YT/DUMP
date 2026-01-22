9baba7a81c1d69a0d8f1b1bb179753a3148a9f90
Pianocraft1YT <lucca.chen@crpusd.org>
2026-01-22 09:20:13 -0800

    1.3.2 done lets go

---
+++ TraverseBackwards.java
@@ -0,0 +1,17 @@
+/*
+ * Activity 3.1.2
+ */
+public class TraverseBackwards
+{
+  public static void main(String[] args)
+  {    
+    int[] goals = {1, 2, 0, 3, 2, 4, 2, 1, 0, 2, 0, 1, 3, 2};
+                   
+    /* your code here */
+    for (int i = 14; (i <= goals.length) && (i != 0); i--){
+    System.out.println("Game " + i + ": " + goals[i-1]);
+      
+      }
+    }
+  }
+
