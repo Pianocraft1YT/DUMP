221f7839189e2c185b953458af849eae010bfeeb
Pianocraft1YT <lucca.chen@crpusd.org>
2026-02-18 16:11:42 -0800

    real revert

--- metadata.py
+++ metadata.py
@@ -17,6 +17,8 @@ video_present = False
 j = 1
 list_of_dates = []
 list_of_images = []
+video_types = [".mp4",".mov",".avi",".mkv",".webm",".wmv"]
+
 def set_dir():
     global directory_path
     directory_path = filedialog.askdirectory(title="Select a Directory")
@@ -34,45 +36,41 @@ def get_exif_metadata():
     global photos_before_video
     try:
         files_and_dirs = os.listdir(directory_path)
-        while (i < len(files_and_dirs)):
-            for file in files_and_dirs:
-                if file.lower().find(".mov") == -1:
-                    # Open the image file
-                    img = Image.open(Path(directory_path) / files_and_dirs[i])
-                    # Get the EXIF data
-                   
-                    exif_data = img.getexif()
-                        
-                    dt = exif_data.get(306) or exif_data.get(36867)
-                    filename = files_and_dirs[i]
-                    list_of_dates.append(dt)
-                    list_of_images.append(filename)
-                    i+=1
-                        
-                else:
-                    if video_present == False:
-                        photos_before_video = i
-                        video_present = True
-                    i+=1
+        files_and_dirs.sort(key=lambda p: os.path.getctime(directory_path + "/" + p))
+        for file in files_and_dirs:
+            if not file.lower().endswith(tuple(video_types)):
+                # Open the image file
+                img = Image.open(Path(directory_path) / files_and_dirs[i])
+                # Get the EXIF data
+                exif_data = img.getexif()
+                dt = exif_data.get(306) or exif_data.get(36867)
+                filename = files_and_dirs[i]                   
+                list_of_dates.append(dt)
+                list_of_images.append(filename)
+                i+=1
+            else:
+                if not video_present:
+                    photos_before_video = i
+                    video_present = True
+                i+=1
                 
     except UnidentifiedImageError as e:
         messagebox.showerror("Image couldn't be scanned.", message="Image couldn't be scanned, is it corrupt? \nError caught: " + str(e) + "\nPlease delete the specified file.")
         return
     except Exception as e:
-        messagebox.showerror("Invalid path(s) specified.", message="Invalid path(s) specified. \nError caught: " + str(e))
+        messagebox.showerror("Invalid path(s) specified.", message="Invalid path(s) specified.")
         return
-    
-    # while j <= len(files_and_dirs):
-    #     if video_present:
-    #         img_series.append(str(j)+"-"+str((j+photos_before_video)))
-    #         j+=(photos_before_video+1)
-    #     else:
-    #         img_series.append(j)
-    #         j+=1
-    unNest(final_images, list_of_images)
-    unNest(final_dates, fixed_date)
-    unNest(final_times, fixed_time)
-    unNest(final_series, img_series)
+    for date in list_of_dates:
+        dateslist = str(date).split(" ",maxsplit=1)
+        fixed_date.append(dateslist[0])
+        fixed_time.append(dateslist[1])
+    while j <= len(files_and_dirs):
+        if video_present:
+            img_series.append(str(j)+"-"+str((j+photos_before_video)))
+            j+=(photos_before_video+1)
+        else:
+            img_series.append(j)
+            j+=1
     df = pd.DataFrame({'Dates': fixed_date})
     df['Date_Datetime'] = pd.to_datetime(df['Dates'], format='%Y:%m:%d')
     formatted_dates = df['Date_Datetime'].dt.strftime('%m/%d/%Y')
