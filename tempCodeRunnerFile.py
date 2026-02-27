74fe67ed6b8c9b0b9eddd4b3ad9234280488655e
Pianocraft1YT <Lucca.chen@crpusd.org>
2026-02-27 09:50:48 -0800

    project 1.1.9 algos and art

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,27 @@
-df = pd.DataFrame({'Files': list_of_images, 'Dates': fixed_date, 'Time': fixed_time})
+repeat = input("Wanna do it again? [yes/no] ")
+        elif randomsize == "no" and repeat == "yes" and randomcolor == "no":
+            block.clear()
+            usersize()
+            usercolors()
+            drawfn()
+            repeat = input("Wanna do it again? [yes/no] ")
+        elif randomcolor == "yes" and randomsize == "yes" and repeat == "yes":
+            randcolorfn()
+            randsizefn()
+            drawfn()
+            repeat = input("Wanna do it again? [yes/no] ")
+        elif randomcolor == "yes" and randomsize == "no" and repeat == "yes":
+#Iterate through the list of possible colors
+            randcolorfn()
+            usersize()
+            drawfn()
+            repeat = input("Wanna do it again? [yes/no] ")
+#If you don't want to, compliments user
+        if repeat == "no":
+            print("Have a nice day!")
+#To prevent from going to else:
+        if repeat == "yes":
+            print("Let's go!")
+#Invalid ansers get rejected
+        else:
+            print("Something went wrong. Try again.")
