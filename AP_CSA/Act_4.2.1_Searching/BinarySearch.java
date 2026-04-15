139cf3894b7cb2cc80bab7c6af20a13df075b192
Pianocraft1YT <lucca.chen@crpusd.org>
2026-04-15 09:42:39 -0700

    oops

---
+++ AP_CSA/Act_4.2.1_Searching/BinarySearch.java
@@ -0,0 +1,35 @@
+
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] data = {2, 5, 8, 12, 16, 23, 38}; // Must be sorted
+        int target = 23;
+        int result = search(data, target);
+        
+        System.out.println(result != -1 ? "Element found at index: " + result : "Element not found");
+    }
+    
+    /**
+    * Returns the index of target if found, else returns -1
+    */
+    public static int search(int[] arr, int target) {
+        int left = 0; //TODO assign to appropriate value
+        int right = arr.length; //TODO assign to appropriate value
+
+        while (left <= right) {
+            // Use this formula to prevent potential integer overflow
+            int mid = (left+right)/2; //TODO assign appropriate, dynamic value
+
+            if (arr[mid] == target) { //TODO replace with appropriate condition
+                return mid; // Target found
+            } else if (arr[mid] < target) { //TODO replace with appropriate condition
+                left = mid+1; //TODO assign to appropriate, dynamic value
+            } else {
+                right = mid-1; //TODO assign to appropriate, dynamic value
+            }
+        }
+        // Target not found 
+        return -1; //TODO assign to appropriate value
+    }
+}
